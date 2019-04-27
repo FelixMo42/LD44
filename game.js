adedab5d9a99724ac881c0f1c1a892f48b602336
FelixMo42 <felix@tacocat.com>
2019-04-26 20:50:26 -0700

    basic structor system

--- game.js
+++ game.js
@@ -7,21 +7,49 @@ var player = {
     y: 0,
 
     speed: 300,
-    moveCost: 1
+    moveCost: 1,
+
+    reach: 100
 }
 
 var buildMenu = [
     {
-        name: "wall"
+        name: "wall",
+        hp: 200,
+        draw: (x, y, angle) => {
+            for (var i = -2; i <= 2; i++) {
+                circle(x + i * 20 * sin(angle), y + i * 20 * cos(angle), 20)
+            }
+        },
+        collide: (x, y, angle, cx, cy, radius) => {
+            for (var i = -2; i <= 2; i++) {
+                var rx = x + i * 20 * sin(angle)
+                var ry = y + i * 20 * cos(angle)
+
+                if ((rx - cx) ** 2 + (ry - cy) ** 2 < (radius + 20) ** 2) {
+                    return true
+                }
+            }
+
+            return false
+        }
     },
     {
-        name: "turret"
+        name: "turret",
+        draw: (x, y, angle) => {
+            circle(x, y, 20)   
+        }
     }
 ]
 
 var selected = 0
 
 var enemies = []
+
+var builds = []
+
+var originX = 0
+var originY = 0
 
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight);
@@ -46,24 +74,53 @@ function draw() {
         drawMenu()
     }
 
+    originX = windowWidth / 2
+    originY = windowHeight / 2
 
-    var originX = windowWidth / 2
-    var originY = windowHeight / 2
+    // draw builds
+    builds.forEach((build) => {
+        stroke("black"),
+        fill("white")
+        build.draw(
+            build.x + originX,
+            build.y + originY,
+            build.angle
+        )
+    })
 
     // draw enemies
     enemies.forEach((enemie) => {
         fill("#B22222")
-        circle(originX + enemie.x, originX + enemie.y, 60)
+        circle(originX + enemie.x, originY + enemie.y, 60)
     })
 
     // draw player
     fill("#483D8B")
-    circle(originX + player.x, originX + player.y, 60)
+    circle(originX + player.x, originY + player.y, 60)
 
     // draw build
-    if (selected > -1 && selected < buildMenu.length) {
+    if (started && selected > -1 && selected < buildMenu.length) {
+        strokeWeight(5)
+        let target = createVector(
+            mouseX - player.x - originX,
+            mouseY - player.y - originY
+        )
+        target.limit(player.reach + 30)
+        let angle = -target.heading()
+        target.x += originX + player.x
+        target.y += originY + player.y
         
+        line(
+            originX + player.x, originY + player.y,
+            target.x, target.y
+        )
+
+        fill(255,255,255,100)
+        stroke(0,0,0,100)
+        buildMenu[selected].draw(target.x, target.y, angle)
     }
+
+    strokeWeight(1)
 
     // draw health bar
     stroke("black")
@@ -149,8 +206,23 @@ function update(dt) {
         if ((enemy.x - player.x) ** 2 + (enemy.y - player.y) ** 2 < 3600) {
             player.hp -= 10
 
-            list.splice(index, 1);
+            list.splice(index, 1)
         }
+
+        builds.forEach((build, i) => {
+            if (build.collide(
+                build.x, build.y, build.angle,
+                enemy.x, enemy.y, 30
+            )) {
+                list.splice(index, 1)
+
+                build.hp -= 10
+
+                if (build.hp < 0) {
+                    builds.splice(i, 1)
+                }
+            }
+        })
     })
 }
 
@@ -163,6 +235,10 @@ function keyPressed() {
     if (keyCode >= 49 && keyCode <= 57) {
         selected = keyCode - 49
     }
+
+    if (keyCode == 27) {
+        selected = -1
+    }
 }
 
 function startUp() {
@@ -172,7 +248,25 @@ function startUp() {
             y: random(-500, 500),
             speed: 100
         })
-     }
+    }
      
     window.setInterval(spawnEnemy, 1000)
+}
+
+function mouseClicked() {
+    let target = createVector(
+        mouseX - player.x - originX,
+        mouseY - player.y - originY
+    )
+    target.limit(player.reach + 30)
+    let angle = -target.heading()
+    target.x += player.x
+    target.y += player.y
+
+    builds.push({
+        ...buildMenu[selected],
+        x: target.x,
+        y: target.y,
+        angle: angle,
+    })
 }
