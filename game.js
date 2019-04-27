fdf483fb9c4162b79a38b32340ed55bcaf58b456
FelixMo42 <felix@tacocat.com>
2019-04-26 21:43:17 -0700

    added turrets

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ var buildMenu = [
     {
         name: "wall",
         hp: 200,
+        cost: 5,
         draw: (x, y, angle) => {
             for (var i = -2; i <= 2; i++) {
                 circle(x + i * 20 * sin(angle), y + i * 20 * cos(angle), 20)
@@ -26,7 +27,7 @@ var buildMenu = [
                 var rx = x + i * 20 * sin(angle)
                 var ry = y + i * 20 * cos(angle)
 
-                if ((rx - cx) ** 2 + (ry - cy) ** 2 < (radius + 20) ** 2) {
+                if ((rx - cx) ** 2 + (ry - cy) ** 2 < (radius + 10) ** 2) {
                     return true
                 }
             }
@@ -36,8 +37,69 @@ var buildMenu = [
     },
     {
         name: "turret",
+        cost: 10,
         draw: (x, y, angle) => {
-            circle(x, y, 20)   
+            push()
+
+            circle(x, y, 50)
+
+            circle(x, y, 20)
+
+            translate(x, y)
+            rotate(-angle)
+
+            rect(0, -10, 20, 5, 5)
+
+            rect(0, +5, 20, 5, 5)
+
+            rotate(0)
+
+            pop()
+        },
+        collide: (x, y, angle, cx, cy, radius) => {
+            return (x - cx) ** 2 + (y - cy) ** 2 < (radius + 25) ** 2
+        },
+        update(self, index, dt) {
+            if (!self.timer) {
+                self.timer = 1
+            }
+            self.timer -= dt
+            if (enemies.length == 0) {
+                return
+            }
+            if (!self.target || self.target.dead) {
+                self.target = enemies[floor(random(0, enemies.length))]
+            }
+
+            this.angle = -createVector(self.target.x - self.x, self.target.y - self.y).heading()
+
+            if (self.timer < 0) {
+                self.timer = 1
+
+                builds.push({
+                    x: self.x,
+                    y: self.y,
+                    angle: self.angle,
+                    timer: 1,
+                    hp: 0,
+                    draw: (x, y, angle) => {
+                        circle(x, y, 20)
+                    },
+                    collide: (x, y, angle, cx, cy, radius) => {
+                        return (x - cx) ** 2 + (y - cy) ** 2 < (radius + 10) ** 2
+                    },
+                    update: (self, index, dt) => {
+                        self.x += cos(self.angle) * 500 * dt
+                        self.y -= sin(self.angle) * 500 * dt
+
+                        self.timer -= dt
+
+                        if (self.timer < 0) {
+                            builds.splice(index, 1)
+                        }
+                    }
+                })
+            }
         }
     }
 ]
@@ -205,7 +267,7 @@ function update(dt) {
 
         if ((enemy.x - player.x) ** 2 + (enemy.y - player.y) ** 2 < 3600) {
             player.hp -= 10
-
+            enemy.dead = true
             list.splice(index, 1)
         }
 
@@ -214,6 +276,7 @@ function update(dt) {
                 build.x, build.y, build.angle,
                 enemy.x, enemy.y, 30
             )) {
+                enemy.dead = true
                 list.splice(index, 1)
 
                 build.hp -= 10
@@ -223,6 +286,13 @@ function update(dt) {
                 }
             }
         })
+    })
+
+    // build updates
+    builds.forEach((build, index) => {
+        if (build.update) {
+            build.update(build, index, dt)
+        }
     })
 }
 
@@ -254,6 +324,8 @@ function startUp() {
 }
 
 function mouseClicked() {
+    if (!started) {return}
+
     let target = createVector(
         mouseX - player.x - originX,
         mouseY - player.y - originY
@@ -262,6 +334,8 @@ function mouseClicked() {
     let angle = -target.heading()
     target.x += player.x
     target.y += player.y
+
+    player.hp -= buildMenu[selected].cost
 
     builds.push({
         ...buildMenu[selected],
